0fbfe315a23963035d29650d1c62d1d96394ff68
MDrance <martindrance@hotmail.com>
2023-02-22 09:13:37 +0100

    unwrapping model

---
+++ run_eval.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export PYTHONPATH=`pwd`
+echo $PYTHONPATH
+
+accelerate launch --config_file accelerate_config.yaml train.py --test --dataset FB15K237 \
+	--beam-size 512 --save-dir "model_1" --ckpt "ckpt_30.pt" \
+	--test-batch-size 8 --encoder --l-punish --no-filter-gen --delf-consistency
