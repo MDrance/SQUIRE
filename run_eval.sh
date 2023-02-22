1eb0d44a9b2ed105a8f78739aa37a159fd29a223
MDrance <martindrance@hotmail.com>
2023-02-22 14:58:47 +0100

    Save/load with accelerate

--- run_eval.sh
+++ run_eval.sh
@@ -4,5 +4,5 @@ export PYTHONPATH=`pwd`
 echo $PYTHONPATH
 
 accelerate launch --config_file accelerate_config.yaml train.py --test --dataset FB15K237 \
-	--beam-size 512 --save-dir "model_1" --ckpt "ckpt_30.pt" \
+	--beam-size 512 --save-dir "model_1" --ckpt "ckpt_4.pt" \
 	--test-batch-size 8 --encoder --l-punish --no-filter-gen --self-consistency
