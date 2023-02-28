bd94604ae6f761c80024f990f5fd804c98a7c96a
MDrance <martindrance@hotmail.com>
2023-02-28 13:38:21 +0100

    generate out paths

--- run_eval.sh
+++ run_eval.sh
@@ -5,4 +5,4 @@ echo $PYTHONPATH
 
 accelerate launch --config_file accelerate_config.yaml train.py --test --dataset FB15K237 \
 	--beam-size 512 --save-dir "model_1" --ckpt "ckpt_30.pt" \
-	--test-batch-size 8 --encoder --l-punish --no-filter-gen --self-consistency
+	--test-batch-size 8 --encoder --l-punish --no-filter-gen --self-consistency --output-path
