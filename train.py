94d76653e99083d4651203f4d9a5fd90c0628121
MDrance <martindrance@hotmail.com>
2023-04-11 14:16:07 +0200

    wait for everyone on iter_training

--- train.py
+++ train.py
@@ -308,6 +308,7 @@ def train(args):
                     train_set.tgt_lines += tgt_lines
                     train_loader = DataLoader(train_set, batch_size=args.batch_size, collate_fn=train_set.collate_fn, shuffle=True)
                     train_loader = accelerator.prepare(train_loader)
+                    # accelerator.wait_for_everyone()
                 # new scheduler
                 step_num = len(train_loader) * curr_iter_epoch
                 warmup_steps = step_num / args.warmup
